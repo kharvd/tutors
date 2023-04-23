54c2072c636b69d2daac3a60071f006e2bb6b526
Shantanu Sharma <shantanusharma@users.noreply.github.com>
2023-04-23 16:59:08 -0400

    Refactors to use backend definitions

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -30,37 +30,38 @@ export default function Question({
     onChoice(index, parseInt(e.target.value));
   };
 
-  return (
+  return (    
     <div className="pt-4">
       <label className="text-base font-semibold text-gray-900">
-        {question.text}
+        {question.question_text}
       </label>
       {/* <p className="text-sm text-gray-500">How do you prefer to receive notifications?</p> */}
       <fieldset className="mt-4">
         <legend className="sr-only">Choices</legend>
         <div className="space-y-4">
-          {question.options.map((option, i) => (
-            <div key={option.text} className="flex items-center">
-              <input
-                id={`q-${index}-o-${i}`}
-                type="radio"
-                name={`question-${index}`}
-                className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
-                value={i}
-                onChange={handleChange}
-              />
-              <label
-                htmlFor={`q-${index}-o-${i}`}
-                className="ml-3 block text-sm font-medium leading-6 text-gray-900"
-              >
-                {option.text} {graded && 
+          {Object.entries(question.choices).map(([i, option]) => {
+            return (
+              <div key={option} className="flex items-center">
+                <input
+                  id={`q-${index}-o-${i}`}
+                  type="radio"
+                  name={`question-${index}`}
+                  className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                  value={i}
+                  onChange={handleChange} />
+                <label
+                  htmlFor={`q-${index}-o-${i}`}
+                  className="ml-3 block text-sm font-medium leading-6 text-gray-900"
+                >
+                  {option} {graded && 
                 option.is_correct && userSelected == i && <span>👍</span>}
                 {graded && 
                 !option.is_correct && userSelected == i && <span>👎</span>}
                 
-              </label>
-            </div>
-          ))}
+                </label>
+              </div>
+            );
+          })}
         </div>
       </fieldset>
     </div>
